780452fab52f73520c657ff400c891c068bbc124
jinsem <jinsem@gmail.com>
2019-08-11 09:47:12 -0700

    Make handler compileable

--- app/egts_connection_handler.go
+++ app/egts_connection_handler.go
@@ -1,8 +1,11 @@
+// This is modified version of this file https://github.com/kuznetsovin/egts/blob/master/cmd/receiver/egts_handler.go
 package main
 
 import (
 	"encoding/binary"
 	egts "github.com/kuznetsovin/egts/pkg/egtslib"
+	//TODO: output format
+	//uuid "github.com/satori/go.uuid"
 	"io"
 	"net"
 	"time"
@@ -24,7 +27,7 @@ func handleRecvPkg(conn net.Conn, producer EgtsProducer) {
 		recvPacket        []byte
 	)
 
-	logger.Debug("Connection is established. Remote address: %s", conn.RemoteAddr())
+	logger.Debug("Соединение установлено. Адрес устройства: %s", conn.RemoteAddr())
 	for {
 	Received:
 		serviceType = 0
@@ -44,7 +47,7 @@ func handleRecvPkg(conn net.Conn, producer EgtsProducer) {
 
 			if headerBuf[0] != protocolVersion {
 				_ = conn.Close()
-				logger.Warnf("Packaged does not meat EGTS speicification. Connection with %s is closed", conn.RemoteAddr())
+				logger.Warnf("Полученный пакет не соответствует спецификации ЕГТС. Соединение с %s будет закрыто", conn.RemoteAddr())
 				return
 			}
 
@@ -54,20 +57,17 @@ func handleRecvPkg(conn net.Conn, producer EgtsProducer) {
 			if bodyLen > 0 {
 				pkgLen += bodyLen + 2
 			}
-			// получаем концовку ЕГТС пакета
 			buf := make([]byte, pkgLen-headerLen)
 			if _, err := io.ReadFull(conn, buf); err != nil {
-				logger.Errorf("Package body parsing error: %v", err)
+				logger.Errorf("Ошибка чтения тела пакета: %v", err)
 				_ = conn.Close()
 				return
 			}
-
-			// формируем порлный пакет
 			recvPacket = append(headerBuf, buf...)
 		case io.EOF:
 			<-connTimer.C
 			_ = conn.Close()
-			logger.Warnf("Соединение %s закрыто по таймауту", conn.RemoteAddr())
+			logger.Warnf("Соединение с %s закрыто по таймауту", conn.RemoteAddr())
 			return
 		default:
 			logger.Errorf("Ошибка при получении: %v", err)
@@ -77,7 +77,8 @@ func handleRecvPkg(conn net.Conn, producer EgtsProducer) {
 
 		logger.Debugf("Принят пакет: %X\v", recvPacket)
 		pkg := egts.Package{}
-		receivedTimestamp := time.Now().UTC().Unix()
+		//TODO: output format
+		//receivedTimestamp := time.Now().UTC().Unix()
 		resultCode, err := pkg.Decode(recvPacket)
 		if err != nil {
 			logger.Warn("Ошибка расшифровки пакета")
@@ -85,7 +86,7 @@ func handleRecvPkg(conn net.Conn, producer EgtsProducer) {
 
 			resp, err := createPtResponse(&pkg, resultCode, serviceType, nil)
 			if err != nil {
-				logger.Errorf("Ошибка сборки ответа EGTS_PT_RESPONSE с ошибкой: %v", err)
+				logger.Errorf("Ошибка сборки ответа EGTS_PT_RESPONSE: %v", err)
 				goto Received
 			}
 			_, _ = conn.Write(resp)
@@ -98,9 +99,10 @@ func handleRecvPkg(conn net.Conn, producer EgtsProducer) {
 			logger.Info("Тип пакета EGTS_PT_APPDATA")
 
 			for _, rec := range *pkg.ServicesFrameData.(*egts.ServiceDataSet) {
-				exportPacket := egtsParsePacket{
-					PacketID: uint32(pkg.PacketIdentifier),
-				}
+				//TODO: output format
+				//exportPacket := egtsParsePacket{
+				//	PacketID: uint32(pkg.PacketIdentifier),
+				//}
 
 				isPkgSave = false
 				packetIDBytes := make([]byte, 4)
@@ -116,7 +118,8 @@ func handleRecvPkg(conn net.Conn, producer EgtsProducer) {
 				serviceType = rec.SourceServiceType
 				logger.Info("Тип сервиса ", serviceType)
 
-				exportPacket.Client = rec.ObjectIdentifier
+				//TODO: output format
+				//exportPacket.Client = rec.ObjectIdentifier
 
 				for _, subRec := range rec.RecordDataSet {
 					switch subRecData := subRec.SubrecordData.(type) {
@@ -137,49 +140,52 @@ func handleRecvPkg(conn net.Conn, producer EgtsProducer) {
 						logger.Debugf("Разбор подзаписи EGTS_SR_POS_DATA")
 						isPkgSave = true
 
-						exportPacket.NavigationTimestamp = subRecData.NavigationTime.Unix()
-						exportPacket.ReceivedTimestamp = receivedTimestamp
-						exportPacket.Latitude = subRecData.Latitude
-						exportPacket.Longitude = subRecData.Longitude
-						exportPacket.Speed = subRecData.Speed
-						exportPacket.Course = subRecData.Direction
-						exportPacket.GUID = uuid.NewV4()
+						//TODO: output format
+						//exportPacket.NavigationTimestamp = subRecData.NavigationTime.Unix()
+						//exportPacket.ReceivedTimestamp = receivedTimestamp
+						//exportPacket.Latitude = subRecData.Latitude
+						//exportPacket.Longitude = subRecData.Longitude
+						//exportPacket.Speed = subRecData.Speed
+						//exportPacket.Course = subRecData.Direction
+						//exportPacket.GUID = uuid.NewV4()
 					case *egts.SrExtPosData:
 						logger.Debugf("Разбор подзаписи EGTS_SR_EXT_POS_DATA")
-						exportPacket.Nsat = subRecData.Satellites
-						exportPacket.Pdop = subRecData.PositionDilutionOfPrecision
-						exportPacket.Hdop = subRecData.HorizontalDilutionOfPrecision
-						exportPacket.Vdop = subRecData.VerticalDilutionOfPrecision
-						exportPacket.Ns = subRecData.NavigationSystem
+						//TODO: output format
+						//exportPacket.Nsat = subRecData.Satellites
+						//exportPacket.Pdop = subRecData.PositionDilutionOfPrecision
+						//exportPacket.Hdop = subRecData.HorizontalDilutionOfPrecision
+						//exportPacket.Vdop = subRecData.VerticalDilutionOfPrecision
+						//exportPacket.Ns = subRecData.NavigationSystem
 
 					case *egts.SrAdSensorsData:
 						logger.Debugf("Разбор подзаписи EGTS_SR_AD_SENSORS_DATA")
-						if subRecData.AnalogSensorFieldExists1 == "1" {
-							exportPacket.AnSensors = append(exportPacket.AnSensors, anSensor{1, subRecData.AnalogSensor1})
-						}
-
-						if subRecData.AnalogSensorFieldExists2 == "1" {
-							exportPacket.AnSensors = append(exportPacket.AnSensors, anSensor{2, subRecData.AnalogSensor2})
-						}
-
-						if subRecData.AnalogSensorFieldExists3 == "1" {
-							exportPacket.AnSensors = append(exportPacket.AnSensors, anSensor{3, subRecData.AnalogSensor3})
-						}
-						if subRecData.AnalogSensorFieldExists4 == "1" {
-							exportPacket.AnSensors = append(exportPacket.AnSensors, anSensor{4, subRecData.AnalogSensor4})
-						}
-						if subRecData.AnalogSensorFieldExists5 == "1" {
-							exportPacket.AnSensors = append(exportPacket.AnSensors, anSensor{5, subRecData.AnalogSensor5})
-						}
-						if subRecData.AnalogSensorFieldExists6 == "1" {
-							exportPacket.AnSensors = append(exportPacket.AnSensors, anSensor{6, subRecData.AnalogSensor6})
-						}
-						if subRecData.AnalogSensorFieldExists7 == "1" {
-							exportPacket.AnSensors = append(exportPacket.AnSensors, anSensor{7, subRecData.AnalogSensor7})
-						}
-						if subRecData.AnalogSensorFieldExists8 == "1" {
-							exportPacket.AnSensors = append(exportPacket.AnSensors, anSensor{8, subRecData.AnalogSensor8})
-						}
+						//TODO: output format
+						//if subRecData.AnalogSensorFieldExists1 == "1" {
+						//	exportPacket.AnSensors = append(exportPacket.AnSensors, anSensor{1, subRecData.AnalogSensor1})
+						//}
+						//
+						//if subRecData.AnalogSensorFieldExists2 == "1" {
+						//	exportPacket.AnSensors = append(exportPacket.AnSensors, anSensor{2, subRecData.AnalogSensor2})
+						//}
+						//
+						//if subRecData.AnalogSensorFieldExists3 == "1" {
+						//	exportPacket.AnSensors = append(exportPacket.AnSensors, anSensor{3, subRecData.AnalogSensor3})
+						//}
+						//if subRecData.AnalogSensorFieldExists4 == "1" {
+						//	exportPacket.AnSensors = append(exportPacket.AnSensors, anSensor{4, subRecData.AnalogSensor4})
+						//}
+						//if subRecData.AnalogSensorFieldExists5 == "1" {
+						//	exportPacket.AnSensors = append(exportPacket.AnSensors, anSensor{5, subRecData.AnalogSensor5})
+						//}
+						//if subRecData.AnalogSensorFieldExists6 == "1" {
+						//	exportPacket.AnSensors = append(exportPacket.AnSensors, anSensor{6, subRecData.AnalogSensor6})
+						//}
+						//if subRecData.AnalogSensorFieldExists7 == "1" {
+						//	exportPacket.AnSensors = append(exportPacket.AnSensors, anSensor{7, subRecData.AnalogSensor7})
+						//}
+						//if subRecData.AnalogSensorFieldExists8 == "1" {
+						//	exportPacket.AnSensors = append(exportPacket.AnSensors, anSensor{8, subRecData.AnalogSensor8})
+						//}
 					case *egts.SrAbsCntrData:
 						logger.Debugf("Разбор подзаписи EGTS_SR_ABS_CNTR_DATA")
 
@@ -187,7 +193,8 @@ func handleRecvPkg(conn net.Conn, producer EgtsProducer) {
 						case 110:
 							// Три младших байта номера передаваемой записи (идет вместе с каждой POS_DATA).
 							binary.BigEndian.PutUint32(packetIDBytes, subRecData.CounterValue)
-							exportPacket.PacketID = subRecData.CounterValue
+							//TODO: output format
+							//exportPacket.PacketID = subRecData.CounterValue
 						case 111:
 							// один старший байт номера передаваемой записи (идет вместе с каждой POS_DATA).
 							tmpBuf := make([]byte, 4)
@@ -198,28 +205,30 @@ func handleRecvPkg(conn net.Conn, producer EgtsProducer) {
 							} else {
 								packetIDBytes = tmpBuf
 							}
-
-							exportPacket.PacketID = binary.LittleEndian.Uint32(packetIDBytes)
+							//TODO: output format
+							//exportPacket.PacketID = binary.LittleEndian.Uint32(packetIDBytes)
 						}
 					case *egts.SrLiquidLevelSensor:
 						logger.Debugf("Разбор подзаписи EGTS_SR_LIQUID_LEVEL_SENSOR")
-						sensorData := liquidSensor{
-							SensorNumber: subRecData.LiquidLevelSensorNumber,
-							ErrorFlag:    subRecData.LiquidLevelSensorErrorFlag,
-						}
+						//TODO: output format
+						//sensorData := liquidSensor{
+						//	SensorNumber: subRecData.LiquidLevelSensorNumber,
+						//	ErrorFlag:    subRecData.LiquidLevelSensorErrorFlag,
+						//}
 
-						switch subRecData.LiquidLevelSensorValueUnit {
-						case "00", "01":
-							sensorData.ValueMm = subRecData.LiquidLevelSensorData
-						case "10":
-							sensorData.ValueL = subRecData.LiquidLevelSensorData * 10
-						}
-
-						exportPacket.LiquidSensors = append(exportPacket.LiquidSensors, sensorData)
+						//switch subRecData.LiquidLevelSensorValueUnit {
+						//case "00", "01":
+						//	sensorData.ValueMm = subRecData.LiquidLevelSensorData
+						//case "10":
+						//	sensorData.ValueL = subRecData.LiquidLevelSensorData * 10
+						//}
+						//
+						//exportPacket.LiquidSensors = append(exportPacket.LiquidSensors, sensorData)
 					}
 				}
 
 				if isPkgSave {
+					//TODO: output format
 					if err := producer.Produce( /*&exportPacket*/ ); err != nil {
 						logger.Error(err)
 					}
@@ -240,7 +249,7 @@ func handleRecvPkg(conn net.Conn, producer EgtsProducer) {
 				logger.Debugf("Отправлен пакет EGTS_SR_RESULT_CODE: %X", resp)
 			}
 		case egts.PtResponsePacket:
-			logger.Printf("Тип пакета EGTS_PT_RESPONSE")
+			logger.Debug("Тип пакета EGTS_PT_RESPONSE")
 		}
 	}
 }
